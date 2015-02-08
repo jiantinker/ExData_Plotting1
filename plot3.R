366db86ceb121fc8170694bd70b0ed133b027541
jiantinker <jiantinker@gmail.com>
2015-02-08 02:10:01 -0600

    add plot3.R

---
+++ plot3.R
@@ -0,0 +1,12 @@
+project <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) 
+project <- project[(project$Date == "1/2/2007") | (project$Date == "2/2/2007"),] 
+write.csv(project, file="project1.csv") 
+data<-read.csv("project1.csv") 
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time, sep=" "), format="%d/%m/%Y %H:%M:%S") 
+png(filename="plot3.png", width=480, height=480)
+plot(data$DateTime, data$Sub_metering_1,type="l",col="black", xlab="", ylab="Energy sub metering") 
+lines(data$DateTime, data$Sub_metering_2, col="red") 
+lines(data$DateTime, data$Sub_metering_3, col="blue")
+cols=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+legend("topright", lwd=1, lty=1, col = c("black", "red", "blue"),legend = cols) 
+dev.off() 
